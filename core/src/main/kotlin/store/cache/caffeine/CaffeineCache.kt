e20c573a430efe54183b8395cca7228557fb2372
jon <lgock2023@gmail.com>
2023-03-23 01:13:42 +0900

    [10] feat: change cache impl

--- core/src/main/kotlin/store/cache/caffeine/CaffeineCache.kt
+++ core/src/main/kotlin/store/cache/caffeine/CaffeineCache.kt
@@ -3,38 +3,29 @@ package io.wafflestudio.truffle.core.store.cache.caffeine
 import com.github.benmanes.caffeine.cache.Caffeine
 import io.wafflestudio.truffle.core.store.cache.Cache
 import io.wafflestudio.truffle.core.store.cache.CacheLoader
+import org.slf4j.LoggerFactory
 import java.time.Duration
 import java.util.Optional
 
 class CaffeineCache<K, V> internal constructor(
-    val name: String,
+    private val name: String,
+    private val loader: CacheLoader<K, V>,
     ttl: Duration,
-    cacheLoader: CacheLoader<K, V>,
     maximumSize: Int,
 ) : Cache<K, V> {
+    private val logger = LoggerFactory.getLogger(javaClass)
     private val caffeineCache = Caffeine.newBuilder()
         .maximumSize(maximumSize.toLong())
         .expireAfterWrite(ttl)
         .build<K, Optional<Any>>()
 
-    private val loader: suspend (K) -> V? = when (cacheLoader) {
-        is CacheLoader.SingleLoader -> cacheLoader.loader
-        is CacheLoader.MultiLoader -> { it -> cacheLoader.multiLoader.invoke(listOf(it))[it]!! }
-    }
-    private val multiLoader: suspend ((Collection<K>) -> Map<K, V>) = when (cacheLoader) {
-        is CacheLoader.SingleLoader -> { keys ->
-            keys.mapNotNull { key -> cacheLoader.loader.invoke(key)?.let { key to it } }
-                .toMap()
-        }
-
-        is CacheLoader.MultiLoader -> cacheLoader.multiLoader
-    }
-
     override suspend fun get(key: K): V? {
         val cached = caffeineCache.getIfPresent(key)
 
         if (cached == null) {
-            val fetched = loader.invoke(key)
+            logger.debug("[CaffeineCache][$name] fetch data with key $key")
+
+            val fetched = loader.load(key)
 
             caffeineCache.put(key, Optional.ofNullable(fetched))
 
@@ -59,7 +50,9 @@ class CaffeineCache<K, V> internal constructor(
             if (missedKeys.isEmpty()) {
                 emptyMap()
             } else {
-                multiLoader.invoke(missedKeys)
+                logger.debug("[CaffeineCache][$name] fetch data with keys $keys")
+
+                loader.multiLoad(missedKeys)
             }
 
         if (fetched.isNotEmpty()) {
--- core/src/main/kotlin/store/cache/caffeine/CaffeineCacheBuilder.kt
+++ core/src/main/kotlin/store/cache/caffeine/CaffeineCacheBuilder.kt
@@ -9,7 +9,7 @@ class CaffeineCacheBuilder(
     private val maximumSize: Int = 1000,
 ) : CacheBuilder {
     override fun <K, V> build(name: String, ttl: Duration, loader: CacheLoader<K, V>): CaffeineCache<K, V> =
-        CaffeineCache(name, ttl, loader, maximumSize)
+        CaffeineCache(loader, ttl, maximumSize)
 
     override fun <K, V> build(name: String, ttl: Duration, loader: suspend (K) -> V?): Cache<K, V> =
         build(name, ttl, CacheLoader.SingleLoader(loader))
